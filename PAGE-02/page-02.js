6463465f01a34f0f3d7bdc2ea0b92cf2bfc9aa9e
Nícolas Felipe Ferreira Souza <133381697+NicolasFelipe16@users.noreply.github.com>
2023-10-13 17:52:20 -0300

    Small JS changes (13/10).

--- PAGE-02/page-02.js
+++ PAGE-02/page-02.js
@@ -1,15 +1,43 @@
 var cards = document.querySelectorAll('.card__content');
 
-var card__titles = document.querySelectorAll('.card__title');
-
 var date = new Date()
 const day = date.getDay();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const fullHour = hour + ':' + minutes;
 
 card = cards[day - 1];
-title = card__titles[day - 1];
 
 card.style.boxShadow = "none"
 card.style.backgroundColor = "var(--color-02)";
 card.style.color = "var(--color-05)";
+card.style.borderTop = "1px solid var(--color-05)";
 
-title.style.borderBottom = "1px solid var(--color-05)";
+var ul = card.querySelector('ul');
+var lis = ul.querySelectorAll('li');
+var liIndex;
+
+var class1 = {'hour':  7, 'minute': 10, 'index': 0};
+var class2 = {'hour':  8, 'minute':  0, 'index': 1};
+var class3 = {'hour':  8, 'minute': 50, 'index': 2};
+var pause  = {'hour':  9, 'minute': 40, 'index':-1};
+var class4 = {'hour':  9, 'minute': 55, 'index': 3};
+var class5 = {'hour': 10, 'minute': 40, 'index': 4};
+var class6 = {'hour': 11, 'minute': 30, 'index': 5};
+var end    = {'hour': 12, 'minute': 20, 'index':-1};
+
+var hours = [class1, class2, class3, pause, class4, class5, class6, end]
+
+for (let i = 0; i < hours.length; i++) {
+    if (hour >= hours[i]['hour'] &&
+    minutes >= hours[i]['minute'])
+    {liIndex = hours[i]['index']}
+}
+
+li = lis[liIndex];
+
+li.style.backgroundColor = "var(--color-03-2)";
+
+console.log(hour)
+console.log(minutes)
+console.log(fullHour)
